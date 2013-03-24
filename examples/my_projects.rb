cb496dbcb0ad6c32e435e95ac460579dd1a376eb
Mark Olson <theothermarkolson@gmail.com>
2013-03-23 20:24:57 -0400

    Change up the example's format a tad

--- examples/my_projects.rb
+++ examples/my_projects.rb
@@ -2,17 +2,17 @@ require 'kickscraper'
 
 p "configuring.."
 Kickscraper.configure do |config|
-	config.email = 'your-kickstarter-email-address@domain.com'
-	config.password = 'This is not my real password. Seriously.'
+    config.email = 'your-kickstarter-email-address@domain.com'
+    config.password = 'This is not my real password. Seriously.'
 end
 
 p "logging in.."
 c = Kickscraper.client
 p "got access token #{Kickscraper.token.gsub(/\w{30}$/, "X" * 30)}"
-puts " A | C |"
-puts "------------------------"
+puts " Active | Met Goal |"
+puts "--------------------"
 c.user.backed_projects.each {|x| 
-	print (x.active? ? ' X |' : '   |')
-	print (x.successful? ? ' X | ' : '   | ')
-	puts x.name
+    print (x.active? ? '    X   |' : '        |')
+    print (x.successful? ? '    X     | ' : '          | ')
+    puts x.name
 }
