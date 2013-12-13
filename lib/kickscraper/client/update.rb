2c5f6cd1d121557d729d2f0689a509a847507488
AJ Kerrigan <kerrigan.aj@gmail.com>
2013-12-13 02:20:31 -0500

    Add attribute accessors for Updates
    Provide sequence number, title and body members.

--- lib/kickscraper/client/update.rb
+++ lib/kickscraper/client/update.rb
@@ -1,5 +1,26 @@
 module Kickscraper
     class Update < Api
-    	
+        attr_accessor :sequence, :title, :body
+
+        def reload!
+            @raw = Kickscraper.client.process_api_url("Update", self.urls.api.update, false)
+            Kickscraper::Update::do_coercion(self)
+        end
+
+        def sequence
+            reload! unless @raw['sequence']
+            @raw['sequence']
+        end
+
+        def title
+            reload! unless @raw['title']
+            @raw['title']
+        end
+
+        def body
+            reload! unless @raw['body']
+            @raw['body']
+        end
+
     end
 end
